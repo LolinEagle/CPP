615bac2d51dcfbe67d7705a852b0ea168ff25fe7
François <francois-lolin@live.fr>
2022-11-08 13:34:06 +0100

    save

--- Module05/ex01/Form.hpp
+++ Module05/ex01/Form.hpp
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: frrusso <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/29 12:37:01 by frrusso           #+#    #+#             */
-/*   Updated: 2022/10/29 12:37:04 by frrusso          ###   ########.fr       */
+/*   Created: 2022/11/08 13:31:00 by frrusso           #+#    #+#             */
+/*   Updated: 2022/11/08 13:31:06 by frrusso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CAT_HPP
-#define CAT_HPP
 
-#include "Animal.hpp"
-
-class Cat : public Animal
-{
-	public:
-		// Constructor & Destructor
-		Cat(void);
-		Cat(const Cat &copy);
-		~Cat();
-		Cat	&operator=(const Cat &copy);
-
-		// Function
-		void	makeSound(void) const;
-};
-
-#endif
